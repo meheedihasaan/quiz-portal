ff9ff51c8c23624af2659e2e0818cf40a61cdcfd
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-15 04:51:32 -0700

    Initial commit

---
+++ src/main/java/com/exam/portal/ExamPortalApplication.java
@@ -0,0 +1,13 @@
+package com.exam.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalApplication.class, args);
+	}
+
+}
