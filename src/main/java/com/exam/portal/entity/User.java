e9a74b6726ebf505d843fdf2fabaf4236a47dbb7
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-16 08:25:40 -0700

    Add User, Role and UserRole entity

---
+++ src/main/java/com/exam/portal/entity/User.java
@@ -0,0 +1,48 @@
+package com.exam.portal.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.exam.portal.constsant.EntityConstant;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = EntityConstant.USER)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+	
+	private String profileImage;
+	
+	private boolean isEnable;
+	
+	private String description; 
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+}
