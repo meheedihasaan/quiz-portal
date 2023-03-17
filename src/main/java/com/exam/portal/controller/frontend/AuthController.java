66972b203cf80a5fff6d8512f95fe02f41eb5ecc
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-17 10:55:18 -0700

    Add Message.java to show success message or error message

--- src/main/java/com/exam/portal/controller/frontend/AuthController.java
+++ src/main/java/com/exam/portal/controller/frontend/AuthController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.exam.portal.entity.User;
+import com.exam.portal.helper.Message;
 
 import jakarta.validation.Valid;
 
@@ -26,15 +28,26 @@ public class AuthController {
 	public String signUpProcess(
 		@Valid @ModelAttribute User user, BindingResult bindingResult,
 		@RequestParam(value = "isAgreed", defaultValue = "false") boolean isAgreed,
+		RedirectAttributes redirectAttributes,
 		Model model
 	) {
-		System.out.println(user.getName()+" "+user.getEmail());
-		if(bindingResult.hasErrors()) {
-			System.out.println(bindingResult.toString());
-			model.addAttribute("user", user);
-			return "frontend/sign-up";
+		try {
+			if(bindingResult.hasErrors()) {
+				System.out.println(bindingResult.toString());
+				model.addAttribute("user", user);
+				return "frontend/sign-up";
+			}
+			
+			if(!isAgreed) {
+				throw new Exception("You have not agreed with terms and condition. Please re-fill the form and check the terms and condition to sign up.");
+			}
+			return "redirect:/sign-up";
 		}
-		return "redirect:/sign-up";
+		catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", new Message("alert-danger", "Something went wrong. "+e.getMessage()));
+			return "redirect:/sign-up";
+		}
 	}
 	
 }
+	
