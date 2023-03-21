4d0f03e94bd0bae6e82a694c29ed2b3253febd92
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-20 21:41:55 -0700

    Add validation for creating new category

--- src/main/java/com/exam/portal/controller/backend/CategoryController.java
+++ src/main/java/com/exam/portal/controller/backend/CategoryController.java
@@ -8,15 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.exam.portal.entity.Category;
 import com.exam.portal.entity.Role;
 import com.exam.portal.entity.User;
+import com.exam.portal.helper.Message;
 import com.exam.portal.service.CategoryService;
 import com.exam.portal.service.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/backend/categories")
@@ -53,6 +60,31 @@ public class CategoryController {
 		model.addAttribute("categoriesActive", "active");
 		model.addAttribute("category", new Category());
 		return "admin-template/new-category";
+	}
+	
+	@PostMapping("/new-category")
+	public String createCategory(
+		@Valid @ModelAttribute Category category, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes,
+		Model model,
+		Principal principal
+	) {
+		loadCommonData(model, principal);
+		model.addAttribute("title", "New Category");
+		model.addAttribute("categoriesActive", "active");
+		try {
+			if(bindingResult.hasErrors()) {
+				model.addAttribute("category", category);
+				return "admin-template/new-category";
+			}
+			
+			redirectAttributes.addFlashAttribute("message", new Message("alert-primary", "Category is created successfully."));
+			return "redirect:/backend/categories/new-category";
+		}
+		catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", new Message("alert-danger", "Something went wrong. "+e.getMessage()));
+			return "redirect:/backend/categories/0";
+		}
 	}
 	
 }
