e178b82efac1421d950c31166897290100fa80bc
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-23 04:01:03 -0700

    Add QuizController.java

---
+++ src/main/java/com/exam/portal/controller/backend/QuizController.java
@@ -0,0 +1,44 @@
+package com.exam.portal.controller.backend;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.exam.portal.entity.Role;
+import com.exam.portal.entity.User;
+import com.exam.portal.service.QuizService;
+import com.exam.portal.service.UserService;
+
+@Controller
+@RequestMapping("/backend/quizzes")
+public class QuizController {
+	
+	@Autowired 
+	private QuizService quizService;
+	
+	@Autowired
+	private UserService userService;
+	
+	public void loadCommonData(Model model, Principal principal) {
+		String email = principal.getName();
+		User user = this.userService.getUserByEmail(email);
+		model.addAttribute("user", user);
+		List<Role> roles = user.getUserRoles().stream().map(userRole-> userRole.getRole()).collect(Collectors.toList());
+		model.addAttribute("role", roles.get(0).getName());
+	}
+
+	@GetMapping("/page={pageNumber}")
+	public String viewQuizzesPage(Model model, Principal principal) {
+		loadCommonData(model, principal);
+		model.addAttribute("title", "Quizzes");
+		model.addAttribute("quizzesActive", "active");
+		return "admin-template/quizzes";
+	}
+	
+}
