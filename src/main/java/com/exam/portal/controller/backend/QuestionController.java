89a9d32e325b919fa28cd505c9a4ede84ea9dd6f
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-28 03:58:30 -0700

    Add validation in new-question.html

--- src/main/java/com/exam/portal/controller/backend/QuestionController.java
+++ src/main/java/com/exam/portal/controller/backend/QuestionController.java
@@ -8,17 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.exam.portal.entity.Question;
 import com.exam.portal.entity.Quiz;
 import com.exam.portal.entity.Role;
 import com.exam.portal.entity.User;
+import com.exam.portal.helper.Message;
 import com.exam.portal.service.QuestionService;
 import com.exam.portal.service.QuizService;
 import com.exam.portal.service.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/backend")
@@ -42,7 +50,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/quizzes/{quizId}/{title}/questions/page={pageNumber}")
-	public String viewAddQuestionPage(@PathVariable int quizId, @PathVariable int pageNumber, Model model, Principal principal) {
+	public String viewQuestionsPage(@PathVariable int quizId, @PathVariable int pageNumber, Model model, Principal principal) {
 		loadCommonData(model, principal);
 		model.addAttribute("title", "Add Question");
 		model.addAttribute("quizzesActive", "active");
@@ -53,6 +61,47 @@ public class QuestionController {
 		Quiz quiz = this.quizService.getQuizById(quizId);
 		model.addAttribute("quiz", quiz);
 		return "admin-template/questions";
+	}
+	
+	@GetMapping("/quizzes/{quizId}/{title}/questions/add")
+	public String viewNewQuestionPage(@PathVariable int quizId, Model model, Principal principal) {
+		loadCommonData(model, principal);
+		model.addAttribute("title", "Add Question");
+		model.addAttribute("quizzesActive", "active");
+		Quiz quiz = this.quizService.getQuizById(quizId);
+		model.addAttribute("quiz", quiz);
+		model.addAttribute("question", new Question());
+		return "admin-template/new-question";
+	}
+	
+	@PostMapping("/quizzes/{quizId}/{title}/questions/add")
+	public String addQuestion(
+		@Valid @ModelAttribute Question question,
+		BindingResult bindingResult,
+		@PathVariable int quizId,
+		@RequestParam(value="answer") String answer,
+		RedirectAttributes redirectAttributes,
+		Model model,
+		Principal principal
+	) {
+		loadCommonData(model, principal);
+		model.addAttribute("title","Add Question");
+		model.addAttribute("quizzesActive", "active");
+		Quiz quiz = this.quizService.getQuizById(quizId);
+		System.out.println(answer);
+		try {
+			if(bindingResult.hasErrors()) {
+				model.addAttribute("question", question);
+				model.addAttribute("quiz", quiz);
+				return "admin-template/new-question";
+			}
+			redirectAttributes.addFlashAttribute("message", new Message("alert-primary", "Question is added successfully."));
+			return "";
+		}
+		catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", new Message("alert-primary", "Something went wrong! "+e.getMessage()));
+			return "";
+		}
 	}
 	
 }
