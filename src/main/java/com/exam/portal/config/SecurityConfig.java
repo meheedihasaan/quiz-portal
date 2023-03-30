9bb6bdd17cbd71228f54283f8b22f572ffd3678d
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-03-30 15:59:13 -0700

    Modify CategoryController.java

--- src/main/java/com/exam/portal/config/SecurityConfig.java
+++ src/main/java/com/exam/portal/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.exam.portal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import com.exam.portal.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	
 	@Bean
