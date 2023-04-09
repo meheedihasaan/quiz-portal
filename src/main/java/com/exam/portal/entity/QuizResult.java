aceb151ba1d58e1dcb83ffaebe90a78606359342
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-09 10:03:15 -0700

    Add QuizResult.java entity and establish relationship

---
+++ src/main/java/com/exam/portal/entity/QuizResult.java
@@ -0,0 +1,37 @@
+package com.exam.portal.entity;
+
+import com.exam.portal.constsant.EntityConstant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = EntityConstant.QUIZ_RESULT)
+public class QuizResult {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private int marks;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Quiz quiz;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private User user;
+	
+}
