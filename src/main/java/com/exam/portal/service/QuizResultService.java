b5cf02c49095f4506675d0db2f622f73b33a7843
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-09 11:31:24 -0700

    Add method to fetch quiz result by user

--- src/main/java/com/exam/portal/service/QuizResultService.java
+++ src/main/java/com/exam/portal/service/QuizResultService.java
@@ -1,9 +1,13 @@
 package com.exam.portal.service;
 
+import org.springframework.data.domain.Page;
+
 import com.exam.portal.entity.QuizResult;
 
 public interface QuizResultService {
 
 	QuizResult createQuizResult(QuizResult quizResult);
+	
+	Page<QuizResult> getQuizResultsByUser(int userId, int pageNumber, int pageSize, String sortBy, String sortDirection);
 	
 }
--- src/main/java/com/exam/portal/service/impl/QuizResultServiceImpl.java
+++ src/main/java/com/exam/portal/service/impl/QuizResultServiceImpl.java
@@ -1,10 +1,17 @@
 package com.exam.portal.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.exam.portal.entity.QuizResult;
+import com.exam.portal.entity.User;
+import com.exam.portal.exception.NotFoundException;
 import com.exam.portal.repository.QuizResultRepository;
+import com.exam.portal.repository.UserRepository;
 import com.exam.portal.service.QuizResultService;
 
 @Service
@@ -12,11 +19,31 @@ public class QuizResultServiceImpl implements QuizResultService {
 	
 	@Autowired
 	private QuizResultRepository quizResultRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public QuizResult createQuizResult(QuizResult quizResult) {
 		QuizResult savedQuizResult = this.quizResultRepository.save(quizResult);
 		return savedQuizResult;
+	}
+
+	@Override
+	public Page<QuizResult> getQuizResultsByUser(int userId, int pageNumber, int pageSize, String sortBy, String sortDirection) {
+		User user = this.userRepository.findById(userId).orElseThrow(()-> new NotFoundException("User not found!"));
+		
+		Sort sort = null;
+		if(sortDirection.equalsIgnoreCase("asc")) {
+			sort = Sort.by(sortBy).ascending();
+		}
+		else if(sortDirection.equalsIgnoreCase("desc")) {
+			sort = Sort.by(sortBy).descending();
+		}
+		
+		Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+		Page<QuizResult> quizResultPage = this.quizResultRepository.findByUser(user, pageable);
+		return quizResultPage;
 	}
 
 }
