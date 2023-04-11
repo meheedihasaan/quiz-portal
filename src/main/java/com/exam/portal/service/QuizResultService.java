36aa5672df963091c8ba91b12a264a5e586bb533
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-11 01:14:40 -0700

    Add QuizResultService.java

---
+++ src/main/java/com/exam/portal/service/QuizResultService.java
@@ -0,0 +1,17 @@
+package com.exam.portal.service;
+
+import org.springframework.data.domain.Page;
+
+import com.exam.portal.entity.QuizResult;
+
+public interface QuizResultService {
+
+	QuizResult createQuizResult(QuizResult quizResult);
+	
+	Page<QuizResult> getQuizResults(int pageNumber, int pageSize, String sortBy, String sortDirection);
+	
+	QuizResult getQuizResultById(int id);
+	
+	Page<QuizResult> getQuizResultsByUser(int userId, int pageNumber, int pageSize, String sortBy, String sortDirection);
+	
+}
