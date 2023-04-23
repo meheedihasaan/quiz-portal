13a992fd6b57f3c69ff851967abfd02e02788552
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-04-22 21:08:23 -0700

    Modify the return type of passwordEncoder() bean

--- src/main/java/com/quiz/portal/config/SecurityConfig.java
+++ src/main/java/com/quiz/portal/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.quiz.portal.security.UserDetailsServiceImpl;
@@ -68,7 +69,7 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(10);
 	}
 	
